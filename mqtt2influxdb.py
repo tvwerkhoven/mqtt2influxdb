9dd3f661637b51609999369ca1d1ab788d16e501
Tim van Werkhoven <tvwerkhoven@github>
2019-09-18 19:46:24 +0200

    Updated pusher to accept tag key/val pairs

--- mqtt2influxdb.py
+++ mqtt2influxdb.py
@@ -9,24 +9,59 @@ import datetime
 import requests
 import logging
 
-MQTT_SERVER_IP="172.16.0.2"
-INFLUX_WRITE_URI="http://rpi3b:8086/write?db=smarthome&precision=s"
+MQTT_SERVER_IP="localhost"
+INFLUX_WRITE_URI="http://localhost:8086/write?db=smarthome&precision=s"
 
 
 def persists(msg):
-    # msg should be like influx/environv2/pm25/state, get measurement and field value to push
-    _, measurement, field, _ = msg.topic.split("/")
-    query = "{} {}={}".format(measurement, field, float(msg.payload))
+    # msg.topic should be like 
+    # influx/<measurement>/[<tagname>/<tagvalue>/]*<field>/state, which is 
+    # converted into 
+    # measurement,[tagname=tagvalue]* field=msg.payload
+
+    # Old code without tags:
+    # _, measurement, field, _ = msg.topic.split("/")
+    # query = "{} {}={}".format(measurement, field, float(msg.payload))
+
+    msgarr = msg.topic.split("/")
+
+    # Check topic has even number of fields
+    if (len(msgarr) % 2 != 0):
+        logging.warn("Malformed topic does not have even number of fields: {}".format(msg.topic))
+        return
+
+    # Check topic starts with 'influx' and ends with 'state'
+    i_influx = msgarr.pop(0)
+    i_state = msgarr.pop()
+    if (i_state != 'state' or i_influx != 'influx'):
+        logging.warn("Malformed topic not ending in 'state' or starting with 'influx': {}".format(msg.topic))
+        return
+
+    # Get measurement & field name
+    i_meas = msgarr.pop(0)
+    i_field = msgarr.pop()
+
+    # Init query, check for any remaining tags
+    query = "{}".format(i_meas)
+
+    for tag_key, tag_val in zip(msgarr[::2], msgarr[1::2]):
+        query += ",{}={}".format(tag_key, tag_val)
+
+    query += " {}={}".format(i_field, float(msg.payload))
+
     logging.info(query)
     r = requests.post(INFLUX_WRITE_URI, data=query, timeout=10)
 
 
 logging.basicConfig(level=logging.INFO)
 client = mqtt.Client()
+client.username_pw_set("username", "passwd")
 
 client.on_connect = lambda self, mosq, obj, rc: self.subscribe("influx/#")
 client.on_message = lambda client, userdata, msg: persists(msg)
 
+logging.info("Connecting to {}:1883".format(MQTT_SERVER_IP))
 client.connect(MQTT_SERVER_IP, 1883, 60)
 
+logging.info("Starting listen loop forever...")
 client.loop_forever()
